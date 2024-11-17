b6d723b965de5d37573b3ba245a9b374e05d53b1
Alec <129782580+alecproj@users.noreply.github.com>
2024-11-18 01:19:19 +0300

    wip: transmit and receive on the button
    Есть проблема в синхронизации приема данных от slave. Master принимает 1й кусок данных при первом запуске модуля, а 2й уже при следующем.

--- ip-module/slave/src/spi_control.v
+++ ip-module/slave/src/spi_control.v
@@ -9,7 +9,9 @@ module spi_control (
     SS,
 
     data_from_master,
-    data_to_master
+    data_to_master,
+    receiveing,
+    transmitting
 );
 
 /********************************************************************
@@ -22,11 +24,16 @@ module spi_control (
 
     output reg [7:0] data_from_master;
     input [7:0] data_to_master;
+    output receiveing;
+    output transmitting;
 
     reg [5:0]                   rx_cnt                  =       0;
     reg [5:0]                   tx_cnt                  =       0;
     reg [`DATA_LENGTH-1:0]      mosi_shift_reg          =       0;
     reg [`DATA_LENGTH-1:0]      miso_shift_reg          =       0;
+
+    reg transmitting_f=0;
+    reg receiveing_f=0;
 
 /*********************************************************************
 *receive data 
@@ -36,36 +43,53 @@ always@(posedge SCLK )
     if(SS)
         mosi_shift_reg <= 0;
     else if(!SS && (rx_cnt < `DATA_LENGTH))
+    begin
         mosi_shift_reg <= {mosi_shift_reg[`DATA_LENGTH-2:0],MOSI}; //MSB -> LSB
+    end
     else
         mosi_shift_reg <= 0;
 
 always@(posedge SCLK )
-    if(SS)
+    if(SS) begin
         rx_cnt <= 0;
+    end
     else if(rx_cnt == `DATA_LENGTH - 1) begin
+        receiveing_f <= 0;
         data_from_master <= mosi_shift_reg;
         rx_cnt <= 0;
     end
-    else 
+    else begin
+        receiveing_f <= 1;
         rx_cnt <= rx_cnt + 1;
+    end
+
+assign receiveing = receiveing_f;
 
 /*******************************************************************
 *transmit data 
 *******************************************************************/
 
 always@(negedge SCLK )
-    if(SS)
+    if(SS) 
+    begin
         tx_cnt <= 0;
-    else if(tx_cnt >= `DATA_LENGTH - 1)begin
+    end
+    else if(tx_cnt >= `DATA_LENGTH - 1)
+    begin
         //miso_shift_reg <= mosi_shift_reg;
         miso_shift_reg <= data_to_master;
         tx_cnt <= 0;
     end
-    else 
+    else begin
+        if (tx_cnt == (`DATA_LENGTH - 2))
+            transmitting_f <= 0;
+        else
+            transmitting_f <= 1;
         tx_cnt <= tx_cnt + 1;
+    end
 
 assign MISO = SS ? 1'bz : miso_shift_reg[`DATA_LENGTH-tx_cnt-1] ;    //MSB -> LSB
+assign transmitting = transmitting_f;
 
 
 endmodule
