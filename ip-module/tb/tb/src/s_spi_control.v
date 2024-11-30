850e7516168592bc3c7ff026ab079d9ab48928ad
Alec <129782580+alecproj@users.noreply.github.com>
2024-12-01 01:47:04 +0300

    wip: starting cleaning with tb

--- ip-module/tb/tb/src/s_spi_control.v
+++ ip-module/tb/tb/src/s_spi_control.v
@@ -7,46 +7,49 @@ module s_spi_control (
     MOSI,
     MISO,
     SS,
+    reset,
 
-    data_from_master,
-    data_to_master,
-    receiveing,
-    transmitting,
-    dbg
+    i_data,
+    o_data,
+    is_receiveing,
+    is_transmitting
 );
 
-/********************************************************************
-*port and variables
-********************************************************************/
     input                       SCLK;
     input                       MOSI;
     input                       SS;
     output                      MISO;
+    input                       reset;
 
-    output [7:0] data_from_master;
-    input [7:0] data_to_master;
-    output receiveing;
-    output transmitting;
-    output reg dbg;
+    output reg [7:0]            i_data;
+    input [7:0]                 o_data;
+    output reg                  is_receiveing;
+    output reg                  is_transmitting;
+
+///////////////////////////////////////////////////////////////////////////
+// Internal Wires/Registers
 
     reg [5:0]                   rx_cnt                  =       0;
     reg [5:0]                   tx_cnt                  =       0;
     reg [`DATA_LENGTH-1:0]      mosi_shift_reg          =       0;
     reg [`DATA_LENGTH-1:0]      miso_shift_reg          =       0;
 
-    reg transmitting_f=1;
-    reg receiveing_f=0;
-    reg [7:0] datareg;
-    reg [7:0] rddata;
+///////////////////////////////////////////////////////////////////////////
+// Reset
 
-initial begin
-    miso_shift_reg = data_to_master;
-    //dbg <= 0;
-end
+always @(negedge reset)
+    begin
+       mosi_shift_reg <= 0;
+       miso_shift_reg <= o_data;
+       i_data <= 0;
+       rx_cnt <= 0;
+       tx_cnt <= 0;
+       is_receiveing <= 1;
+       is_transmitting <= 1;    
+    end
 
-/*********************************************************************
-*receive data 
-*********************************************************************/
+///////////////////////////////////////////////////////////////////////////
+// Receive data 
 
 always@(posedge SCLK )
     if(SS)
@@ -58,57 +61,44 @@ always@(posedge SCLK )
     else
         mosi_shift_reg <= 0;
 
-always@(posedge SCLK )
+always@(posedge SCLK or posedge SS)
     if(SS) begin
         rx_cnt <= 0;
+        is_receiveing <= 0;
     end
     else if(rx_cnt == `DATA_LENGTH - 1) begin
-        //receiveing_f <= 0;
-        dbg <= ~dbg;
-        //rddata <= datareg;
-        //datareg <= mosi_shift_reg;
         rx_cnt <= 0;
     end
     else begin
-        receiveing_f <= 1;
+        is_receiveing <= 1;
         rx_cnt <= rx_cnt + 1;
     end
 
 always@(posedge SS)
     begin
-        datareg <= mosi_shift_reg;
-        receiveing_f <= 0;
-        //transmitting_f <= 0;
-        miso_shift_reg <= data_to_master;
+        i_data <= mosi_shift_reg;
+        miso_shift_reg <= o_data;
     end
 
-assign receiveing = receiveing_f;
-assign data_from_master = datareg;
-
-/*******************************************************************
-*transmit data 
-*******************************************************************/
+///////////////////////////////////////////////////////////////////////////
+// Transmit data 
 
 always@(negedge SCLK )
     if(SS) 
     begin
-        // transmitting_f <= 0;
         tx_cnt <= 0;
     end // передача начинается, когда tx_cnt = 0. Обновить данные нужно еще до этого момента.
     else if(tx_cnt >= `DATA_LENGTH - 1) // а заканчивается в этот момент
     begin
-        //miso_shift_reg <= mosi_shift_reg;
-        transmitting_f <= 0;
-        //miso_shift_reg <= data_to_master;
+        is_transmitting <= 0;
         tx_cnt <= 0;
     end
     else begin
-        transmitting_f <= 1;
+        is_transmitting <= 1;
         tx_cnt <= tx_cnt + 1;
     end
 
 assign MISO = SS ? 1'bz : miso_shift_reg[`DATA_LENGTH-tx_cnt-1] ;    //MSB -> LSB
-assign transmitting = transmitting_f;
 
 
 endmodule
