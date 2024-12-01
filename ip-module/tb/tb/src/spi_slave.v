b75d2e0285e002fca045681b087915589cf21d19
Alec <129782580+alecproj@users.noreply.github.com>
2024-12-01 13:00:36 +0300

    wip: fixed multiply definition tb

--- ip-module/tb/tb/src/spi_slave.v
+++ ip-module/tb/tb/src/spi_slave.v
@@ -26,10 +26,11 @@ module spi_slave (
 // Internal Wires/Registers
 
  wire                      reset_on_1;
- wire                      reset_on_0;
  reg  [7:0]                delay_btn_reset=0;
  reg  [14:0]               counter0=0;
  reg                       clk_en=0; 
+
+ reg                       transmit_rd=0;
 
  reg [7:0] o_data = 8'b11111111;
  wire [7:0] i_data;
@@ -38,7 +39,6 @@ module spi_slave (
 // Button debounce processing
 
  assign reset_on_1=&{delay_btn_reset[5],!delay_btn_reset[4],!delay_btn_reset[3],!delay_btn_reset[2],!delay_btn_reset[1],!delay_btn_reset[0]};
- assign reset_on_0=~reset_on_1;
 
  always @(posedge clk) 
     if(counter0==15'd26999) 
@@ -61,15 +61,19 @@ module spi_slave (
 //////////////////////////////////////////////////////////////////////////
 // Data management
 
-always @(posedge reset_on_1) begin
-    o_data <= 8'b11111111;
-end    
-
 always @(negedge is_receiveing)
     leds <= i_data;
-   
-always @(negedge is_transmitting)
-    o_data <= o_data - 1'b1;
+
+always @(posedge clk)
+    if (reset_on_1)
+        o_data <= 8'b11111111;
+    else if (!transmit_rd && !is_transmitting)
+    begin
+        o_data <= o_data - 1'b1;
+        transmit_rd <= 1;
+    end    
+    else if (is_transmitting)
+        transmit_rd <= 0;  
 
 //////////////////////////////////////////////////////////////////////////
 
@@ -78,7 +82,6 @@ s_spi_control u_spi_control(
  .MOSI(MOSI),
  .MISO(MISO),
  .SS(SS),
- .reset(reset_on_0),
  
  .i_data(i_data),
  .o_data(o_data),
