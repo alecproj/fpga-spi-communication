0716136fa21d2ff4f0520d77f3508392b390a84b
Alec <129782580+alecproj@users.noreply.github.com>
2024-12-30 00:42:24 +0300

    feat: 64 bits transmittion MASTER and SLAVE

---
+++ 64SLAVE/src/s_64spi.v
@@ -0,0 +1,113 @@
+`timescale 1ns/1ps
+
+`define DATA_LENGTH 32
+
+module s_64spi
+(
+    clk,
+    SCLK,
+    MOSI,
+    SS,
+    MISO,
+    in,
+    out,
+    reset,
+    status
+);
+
+    input clk;
+    input SCLK;
+    input MOSI;
+    input SS;
+    output MISO;
+    output reg [63:0] in;
+    input  [63:0] out;
+    input  reset;
+    output reg status;
+
+////////////////////////////////////////////////////////////////   
+// Internal Wires/Registers
+    
+    wire                      reset;
+
+    // For data
+    reg   [`DATA_LENGTH-1:0]   i_splited [1:0];
+    wire  [`DATA_LENGTH-1:0]   o_splited [1:0];
+    reg   [`DATA_LENGTH-1:0]   o_data=0;
+    wire [`DATA_LENGTH-1:0]    i_data;
+    reg                        index=0;
+
+    wire                       is_transmitting;
+    wire                       is_receiveing;
+
+    reg   [1:0]                 stage=0;
+
+//////////////////////////////////////////////////////////////////////////
+
+assign o_splited[0] = out[31:0];
+assign o_splited[1] = out[63:32];
+
+always @(posedge reset or posedge clk)
+begin
+	if(reset)
+	begin
+        o_data <= 0;
+        index <= 0;
+        stage <= 0;
+        status <= 0;
+        in <= 0;
+	end
+	else
+	begin
+		if(stage==0)begin    // preparation
+            if(!is_transmitting && !is_receiveing)
+                o_data <= o_splited[index]; 
+            else
+            begin
+                status <= 1;
+                stage <= 1;
+            end
+                        
+		end //if(stage==0)
+
+        else if(stage==1)begin   // receiving
+            if(!is_transmitting && !is_receiveing)
+            begin
+                i_splited[index] <= i_data;
+                stage <= 2;
+            end
+                  
+		end //if(stage==1)
+
+        else if(stage==2)begin   // distribution
+		      if (index==1) // 64 bits received
+                begin
+                    index <= 0;
+                    in[31:0] <= i_splited[0]; 
+                    in[63:32] <= i_splited[1];
+                    stage <= 0;
+                    status <= 0;
+                end
+              else // 32 bits received
+                begin
+                    index = index + 1;
+                    stage <= 0;
+                end
+		end //if(stage==2)
+    end
+end
+
+///////////////////////////////////////////////////////////////////////////
+
+s_spi_control u_spi_control(
+ .SCLK                  ( SCLK              ),
+ .MOSI                  ( MOSI              ),
+ .MISO                  ( MISO              ),
+ .SS                    ( SS                ),
+ .i_data                ( i_data            ),
+ .o_data                ( o_data            ),
+ .is_receiveing         ( is_receiveing     ),
+ .is_transmitting       ( is_transmitting   )
+); 
+
+endmodule
---
+++ 64SLAVE/src/s_top.v
@@ -0,0 +1,49 @@
+`timescale 1ns/1ps
+
+module s_top
+(
+    clk,
+    SCLK,
+    MOSI,
+    SS,
+    MISO,
+    btn_reset
+);
+
+    input clk;
+    input SCLK;
+    input MOSI;
+    input SS;
+    output MISO;
+    input  btn_reset;
+
+////////////////////////////////////////////////////////////////   
+// Internal Wires/Registers
+    
+    wire reset;
+    wire status;
+
+    wire [63:0] i_float;
+    reg  [63:0] o_float=64'h3333333366666666;
+
+///////////////////////////////////////////////////////////////////////////
+
+s_btn_control u_btn_control (
+    .clk                   ( clk            ),
+    .btn_reset             ( btn_reset      ),
+    .reset                 ( reset          )
+);
+
+s_64spi u_64_spi (
+    .clk                   ( clk            ),
+    .SCLK                  ( SCLK           ),
+    .MOSI                  ( MOSI           ),
+    .MISO                  ( MISO           ),
+    .SS                    ( SS             ),
+    .in                    ( i_float        ),
+    .out                   ( o_float        ),
+    .reset                 ( reset          ),
+    .status                ( status         )
+);
+
+endmodule
